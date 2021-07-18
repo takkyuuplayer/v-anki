f86e32da46b3fae7a93d458ff397bcac20b76224
takkyuuplayer <takkyuuplayer@gmail.com>
2021-07-18 22:49:32 +0900

    use dotenv

--- cmd/cli.v
+++ cmd/cli.v
@@ -5,8 +5,10 @@ import dictionary
 import dictionary.mw
 import envars
 import os
+import zztkm.vdotenv
 
 fn main() {
+	vdotenv.load()
 	env := envars.load() ?
 
 	mut dictionaries := []dictionary.Dictionary{}
