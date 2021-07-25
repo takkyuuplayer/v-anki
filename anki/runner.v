0c46a97ee0f2ae58dd9b50ae440e16d070cc557b
takkyuuplayer <takkyuuplayer@gmail.com>
2021-07-25 23:24:44 +0900

    to avoid infinite wait

--- anki/runner.v
+++ anki/runner.v
@@ -27,18 +27,22 @@ pub fn (r Runner) run(reader io.Reader, writer io.Writer, err_writer io.Writer) 
 	mut wg := sync.new_waitgroup()
 	mut mu := sync.new_mutex()
 
+	mut counter := 0
 	for {
 		word := (br.read_line() or { break }).trim_space()
 		if word == '' {
 			continue
 		}
+		counter++
 		ch <- true
 		wg.add(1)
 
 		go r.run_on_word(writer, err_writer, word, ch, mut wg, mut mu)
 	}
 
-	wg.wait()
+	if counter > 0 { // to avoid infinte wait() in case of no words.
+		wg.wait()
+	}
 }
 
 fn (r Runner) run_on_word(writer io.Writer, err_writer io.Writer, word string, ch chan bool, mut wg sync.WaitGroup, mut mu sync.Mutex) {
